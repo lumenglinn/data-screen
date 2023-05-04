d4aae294030360ba21af889fb91fbc1601d3b4b0
lumenglinn <35032663+lumenglinn@users.noreply.github.com>
2023-05-04 18:54:03 +0800

    Update utils.js

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,7 +1,39 @@
 /* Echarts图表字体、间距自适应  */
 export const echartsSize = (size, defalteWidth = 1920) => {
-  let clientWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+  let clientWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth
   if (!clientWidth) return size
-  let scale = (clientWidth / defalteWidth)
+  let scale = clientWidth / defalteWidth
   return Number((size * scale).toFixed(3))
 }
+
+// 全屏
+export const fullScreen = () => {
+  const de = document.documentElement
+  if (de.requestFullscreen) {
+    de.requestFullscreen()
+  } else if (de.mozRequestFullScreen) {
+    de.mozRequestFullScreen()
+  } else if (de.webkitRequestFullScreen) {
+    de.webkitRequestFullScreen()
+  }
+}
+
+// 退出全屏
+export const exitFullscreen = () => {
+  if (document.exitFullscreen) {
+    document.exitFullscreen()
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen()
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen()
+  }
+}
+
+// 退出全屏
+export const closeWindow = () => {
+  window.close()
+}
+
