a856ad31e99500080c6f10f9a8243b63c579f334
lumenglinn <35032663+lumenglinn@users.noreply.github.com>
2023-05-04 13:56:42 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
 import router from './router'
-import echarts from './common/echarts.js'
+// import echarts from './common/echarts.js'
 // import piniaPluginPersist from 'pinia-plugin-persist'
 // import { createPinia } from 'pinia'
 // import { ajaxRequest } from '@/common/api/index'
@@ -12,6 +12,6 @@ const app = createApp(App)
 // pinia.use(piniaPluginPersist)
 // app.config.globalProperties.$http = ajaxRequest
 
-app.config.globalProperties.$echarts = echarts;
+// app.config.globalProperties.$echarts = echarts;
 
 app.use(router).mount('#app')
