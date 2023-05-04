f48ec1c2f86453e144661bc87a8976aedec54b90
lumenglinn <35032663+lumenglinn@users.noreply.github.com>
2023-05-04 13:59:56 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import router from './router'
 // import piniaPluginPersist from 'pinia-plugin-persist'
 // import { createPinia } from 'pinia'
 // import { ajaxRequest } from '@/common/api/index'
+import { echartsSize } from './common/utils.js'
 
 const app = createApp(App)
 // const pinia = createPinia()
 // pinia.use(piniaPluginPersist)
 // app.config.globalProperties.$http = ajaxRequest
-
 // app.config.globalProperties.$echarts = echarts;
-
+app.config.globalProperties.echartsSize = echartsSize
 app.use(router).mount('#app')
