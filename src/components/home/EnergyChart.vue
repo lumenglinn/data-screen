6a9c3c9ddf79bef48c674d53a5007bd3109f8dc4
lumenglinn <35032663+lumenglinn@users.noreply.github.com>
2023-05-04 18:51:45 +0800

    Update EnergyChart.vue

--- src/components/home/EnergyChart.vue
+++ src/components/home/EnergyChart.vue
@@ -1,43 +1,42 @@
 
 
 <template>
-  <div ref="EcharRef" style="width: 330px; height: 145px"></div>
+  <div ref="EcharRef" class="my-charts"></div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import * as echarts from "echarts";
 const EcharRef = ref(null)
+const { proxy } = getCurrentInstance()
+
+const props = defineProps({
+  dataSource: {
+    type: Array,
+    default: []
+  }
+})
 
 onMounted(() => {
-  const myChars = echarts.init(EcharRef.value);
-  myChars.setOption({
+  const myChart = proxy.$echarts.init(EcharRef.value);
+  myChart.setOption({
     xAxis: {
       type: 'category',
       axisLabel: {
         interval: 1,
         textStyle: {
-          fontSize: 10,
+          fontSize: proxy.$echartsSize(10),
           color: '#fff'
         }
       },
       axisTick: {
         interval: 'auto'
-        // lineStyle: {
-        //   color: '#FFFFFF',
-        //   width: 1,
-        // }
       },
       boundaryGap: false,
       data: ['08:00', '08:00', '10:00', '10:00', '12:00', '12:00', '14:00', '14:00', '16:00', '16:00', '18:00'],
-      // show: false,
-      // data: ['08:00', '10:00', '12:00', '14:00', '16:00', '18:00'],
     },
     yAxis: {
       type: 'value',
-      // inverse: true,
       axisLabel: {
-        fontSize: 12,
+        fontSize: proxy.$echartsSize(12),
         color: "#ffffff"
       },
       minInterval: 1,
@@ -48,12 +47,11 @@ onMounted(() => {
         }
       },
       axisTick: { show: false },
-      // data: ['沙溪北区太仓工厂店', '沙溪南区', '沙溪北区工厂', '沙溪北区太仓工厂店', '沙溪北区太仓工厂店', '沙溪南区', '沙溪北区工厂', '沙溪北区太仓工厂店']
     },
     grid: {
-      top: 45,
-      bottom: 30,
-      left: 50,
+      top: proxy.$echartsSize(45),
+      bottom: proxy.$echartsSize(30),
+      left: proxy.$echartsSize(50),
       show: false
     },
     axisTick: {
@@ -66,14 +64,12 @@ onMounted(() => {
     },
     series: [
       {
-        data: [1, 1.2, 1.5, 1.3, 2, 2.5, 2.2, 3, 2.5, 2.1, 2],
+        data: props.dataSource,
         type: 'bar',
-        // showBackground: true,
         itemStyle: {
           barBorderRadius: 15,
-          // shadowBlur: 3,
           normal: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+            color: new proxy.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
               offset: 0,
               color: '#50E4DF'
             }, {
