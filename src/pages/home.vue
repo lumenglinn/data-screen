2f90133282c2ab70434c2d0d08dfabfd83ef6a50
lumenglinn <35032663+lumenglinn@users.noreply.github.com>
2023-05-06 18:17:16 +0800

    Update home.vue

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,193 +1,31 @@
 <template>
   <div class="home-page">
     <div class="content">
-      <div class="main-data">
-        <div class="data-item">
-          <img class="data-icon" src="../assets/home/top1.png" alt="">
-          <div class="data-right">
-            <div class="data-name">当日总销售</div>
-            <count-to :start-val="0" :end-val="topData.data" class="data" :duration='1000' />
-          </div>
+      <!-- <div class="top-handle-wrap">
+        <div class="handle-box">
+          <div class="handle-item" :class="{ 'active': area === 'AB' }" @click="switchArea('AB')">AB区</div>
+          <div class="handle-item" :class="{ 'active': area === 'A' }" @click="switchArea('A')">A区</div>
+          <div class="handle-item" :class="{ 'active': area === 'B' }" @click="switchArea('B')">B区</div>
         </div>
-        <div class="data-item">
-          <img class="data-icon" src="../assets/home/top2.png" alt="">
-          <div class="data-right">
-            <div class="data-name">能源销售</div>
-            <count-to :start-val="0" :end-val="topData.data" class="data" :duration='1000' />
-          </div>
+        <div class="handle-box">
+          <div class="handle-item" :class="{ 'active': field === 'inField' }" @click="switchField('inField')">内场</div>
+          <div class="handle-item" :class="{ 'active': field === 'outField' }" @click="switchField('outField')">外场</div>
         </div>
-        <div class="data-item">
-          <img class="data-icon" src="../assets/home/top3.png" alt="">
-          <div class="data-right">
-            <div class="data-name">零售销售</div>
-            <count-to :start-val="0" :end-val="topData.data" class="data" :duration='1000' />
-          </div>
-        </div>
-        <div class="data-item">
-          <img class="data-icon" src="../assets/home/top4.png" alt="">
-          <div class="data-right">
-            <div class="data-name">商户销售</div>
-            <count-to :start-val="0" :end-val="topData.data" class="data" :duration='1000' />
-          </div>
-        </div>
-        <div class="data-item">
-          <img class="data-icon" src="../assets/home/top5.png" alt="">
-          <div class="data-right">
-            <div class="data-name">车流情况</div>
-            <count-to :start-val="0" :end-val="topData.data" class="data" :duration='1000' />
-          </div>
-        </div>
-        <div class="data-item">
-          <img class="data-icon" src="../assets/home/top6.png" alt="">
-          <div class="data-right">
-            <div class="data-name">客流情况</div>
-            <count-to :start-val="0" :end-val="topData.data" class="data" :duration='1000' />
-          </div>
-        </div>
-      </div>
-      <div class="ratio-wrap">
-        <div class="ratio-box">
-          <div class="data f-w-900">
-            <div>18%</div>
-            <img class="icon-up-down" src="../assets/home/down.png" alt="" />
-            <div>0%</div>
-          </div>
-          <div class="ratio-name">入区率</div>
-        </div>
-        <div class="ratio-box">
-          <div class="data f-w-900">8:2</div>
-          <div class="ratio-name">客货比</div>
-        </div>
-        <div class="ratio-box">
-          <div class="data">
-            <div class="f-w-900">20</div>分
-            <img class="icon-up-down" src="../assets/home/up.png" alt="" />
-            <div class="f-w-900">0%</div>
-          </div>
-          <div class="ratio-name">平均停留时间</div>
-        </div>
-      </div>
+      </div> -->
+      <home-top />
+      <home-ratio :ratioData="ratioData" />
       <div :class="[footerShow ? 'animate__fadeInLeft' : 'animate__fadeOutLeft', isInitAnimated ? 'handle-duration' : '']"
         class="content-left animate__animated">
-        <div class="wrap">
-          <div class="wrap-title">商铺销售</div>
-          <div class="wrap-cont">
-            <div class="chart-handle">
-              <div class="handle-box">
-                <div class="handle-item">年</div>
-                <div class="handle-item active">月</div>
-                <div class="handle-item">日</div>
-              </div>
-            </div>
-            <shop-chart class="bar-chart" :dataSource="shopData" />
-          </div>
-        </div>
-        <div class="wrap">
-          <div class="wrap-title">服务质量</div>
-          <div class="info_list_content swiper swiper-container swiper1">
-            <div class="swiper-wrapper">
-              <div class="swiper-slide">
-                <div v-for="(item, index) in infoList" :key="index">
-                  <rh-image :src="item.src" :width="item.width" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="wrap-cont wrap-2">
-            <services :dataSource="serviceData" />
-          </div>
-        </div>
-        <div class="wrap">
-          <div class="wrap-title">客流</div>
-          <div class="wrap-cont">
-            <div class="chart-handle">
-              <div class="handle-box">
-                <div class="handle-item">内场</div>
-                <div class="handle-item active">外场</div>
-              </div>
-              <div class="handle-box">
-                <div class="handle-item">年</div>
-                <div class="handle-item active">月</div>
-                <div class="handle-item">日</div>
-              </div>
-            </div>
-            <passenger-chart :dataSource="passengerData" />
-          </div>
-        </div>
+        <shop-chart class="bar-chart" :dataSource="shopData" ref="shopChartRef" />
+        <services :dataSource="serviceData" ref="serviceRef" />
+        <passenger-chart :dataSource="passengerData" ref="passengerRef" />
       </div>
       <div
         :class="[footerShow ? 'animate__fadeInRight' : 'animate__fadeOutRight', isInitAnimated ? 'handle-duration' : '']"
         class="content-right animate__animated">
-        <div class="wrap">
-          <div class="wrap-title">车流</div>
-          <div class="wrap-cont">
-            <div class="chart-handle">
-              <div class="handle-box">
-                <div class="handle-item">年</div>
-                <div class="handle-item active">月</div>
-                <div class="handle-item">日</div>
-              </div>
-            </div>
-            <car-chart :dataSource="carData" />
-          </div>
-        </div>
-        <div class="wrap energy">
-          <div class="wrap-title">能耗监测</div>
-          <div class="energy-box">
-            <div class="energy-item">
-              <div class="item-content">
-                <div class="energy-left">今日用水:14.60t</div>
-                <div class="energy-right"><img class="icon-up-down" src="../assets/home/down.png" alt="" />0%</div>
-              </div>
-            </div>
-            <div class="energy-item">
-              <div class="item-content">
-                <div class="energy-left">今日用水:14.60t</div>
-                <div class="energy-right"><img class="icon-up-down" src="../assets/home/down.png" alt="" />0%</div>
-              </div>
-            </div>
-          </div>
-          <div class="wrap-cont">
-            <div class="chart-handle">
-              <div class="handle-box">
-                <div class="handle-item">内场</div>
-                <div class="handle-item active">外场</div>
-              </div>
-              <div class="handle-box">
-                <div class="handle-item">年</div>
-                <div class="handle-item active">月</div>
-                <div class="handle-item">日</div>
-              </div>
-            </div>
-            <energy-chart :dataSource="energyData" />
-          </div>
-        </div>
-        <div class="wrap equipment">
-          <div class="wrap-title">设备监测</div>
-          <div class="equipment-box">
-            <div class="equipment-item">
-              <div class="item-content">
-                <div>总数</div>
-                <div class="equipment-data">999</div>
-              </div>
-            </div>
-            <div class="equipment-item">
-              <div class="item-content">
-                <div>在线率</div>
-                <div class="equipment-data">79%</div>
-              </div>
-            </div>
-            <div class="equipment-item">
-              <div class="item-content">
-                <div>故障</div>
-                <div class="equipment-data error">6</div>
-              </div>
-            </div>
-          </div>
-          <div class="wrap-cont">
-            <equipment />
-          </div>
-        </div>
+        <car-chart :dataSource="carData" ref="carChartRef" />
+        <energy-chart :dataSource="energyData" :energyObj="energyObj" ref="energyChartRef" />
+        <equipment ref="equipmentRef" />
       </div>
       <div class="handle-content" @click="handleContent"></div>
     </div>
@@ -195,14 +33,23 @@
 </template>
 
 <script setup>
-import { reactive } from "vue";
-import { numFormat } from '@/common/utils'
-import countTo from "@/components/CountTo";
+import { onMounted, reactive } from "vue";
 
+const shopChartRef = ref(null)
+const serviceRef = ref(null)
+const passengerRef = ref(null)
+const carChartRef = ref(null)
+const energyChartRef = ref(null)
+const equipmentRef = ref(null)
+
+const shopData = reactive({
+  yData: ['沙溪北区太仓工厂店', '沙溪南区', '沙溪北区工厂', '沙溪北区太仓工厂店', '沙溪北区太仓工厂店', '沙溪南区', '沙溪北区工厂', '沙溪北区太仓工厂店', '沙溪北区太仓工厂店', '沙溪南区', '沙溪北区工厂', '沙溪北区太仓工厂店'],
+  data: [0.55, 0.42, 0.42, 0.4, 0.35, 0.29, 0.2, 0.1, 0.35, 0.29, 0.2, 0.1]
+})// 商铺图表数据
 const carData = reactive([20, 40, 30, 45, 55, 50, 40, 20, 25, 30, 25, 10])// 车流图表数据
 const energyData = reactive([1, 1.2, 1.5, 1.3, 2, 2.5, 2.2, 3, 2.5, 2.1, 2])// 能源图表数据
 const passengerData = reactive([200, 300, 360, 600, 450, 800, 1200, 1600, 1450, 1500, 1780, 1690]) // 客流图表数据
-const shopData = reactive([0.55, 0.42, 0.42, 0.4, 0.35, 0.29, 0.2, 0.1])// 商铺图表数据
+// 服务质量数据
 const serviceData = reactive([
   { no: '193829199', name: '消防灭火器', time: '2023-01-01 12:00', result: 1 },
   { no: '193829199', name: '员工餐厅', time: '2023-01-01 12:00', result: 1 },
@@ -214,224 +61,118 @@ const serviceData = reactive([
   { no: '193829199', name: '公共场所', time: '2023-01-01 12:00', result: 2 },
   { no: '193829199', name: '办公及生活', time: '2023-01-01 12:00', result: 2 },
   { no: '193829199', name: '出售的商品', time: '2023-01-01 12:00', result: 2 },
-])// 服务质量数据
+])
+
 // 顶部数据
 const topData = reactive({
   data: 1900,
 })
-const swiperOptions = reactive({})
 
+// 右边比率数据
+const ratioData = reactive({
+  val: 9
+})
+
+// 能耗数据
+const energyObj = reactive({
+  val: 9
+})
+
+const field = ref('outField') // 场内/场外
+const area = ref('A') // A区/B区/AB区
 const footerShow = ref(true)
 const isInitAnimated = ref(false)
+let timer = null
+
+// 刷新页面数据
+watch([field, area], (newVal, oldVal) => {
+  if (timer) clearInterval(timer)
+  initData()
+})
+
+onMounted(() => {
+  initData()
+})
+
+onUnmounted(() => {
+  if (timer) clearInterval(timer)
+});
+
+const initData = () => {
+  getRatioData()
+  getMainData()
+  getShopData()
+  getCarData()
+  getServerData()
+  getEnergyData()
+  getPassengerData()
+  getEquipmentData()
+  refreshData()
+}
+
+// 5分钟刷新
+const refreshData = () => {
+  timer = setInterval(async () => {
+    await getRatioData()
+    await getMainData()
+    await getShopData()
+    await getCarData()
+    await getServerData()
+    await getEnergyData()
+    await getPassengerData()
+    await getEquipmentData()
+  }, 300000)
+}
+
+// 获取商铺数据
+async function getShopData () {
+  const params = {
+    field: field.value,
+    area: area.value
+  }
+  console.log(params)
+  // console.log(sonRef.value)
+  const res = await proxy.$http('post', 'getInfoForWeb', params)
+}
+
+// 获取服务质量数据
+async function getServerData () {}
+
+// 获取右上角比率数据
+async function getRatioData () { }
+
+// 获取上方数据
+async function getMainData () { }
+
+// 获取客流数据
+async function getPassengerData () { }
+
+// 获取车流数据
+async function getCarData () { }
+
+// 获取能耗监测数据
+async function getEnergyData () { }
+
+// 获取设备监测数据
+async function getEquipmentData () { }
 
 const handleContent = () => {
   isInitAnimated.value = true
   footerShow.value = !footerShow.value
 }
 
+const switchArea = (val) => {
+  area.value = val
+}
+
+const switchField = (val) => {
+  field.value = val
+}
+
 </script>
 
 <style lang="scss" scoped>
 .home-page {
-  .main-data {
-    width: 900px;
-    position: absolute;
-    top: 135px;
-    left: 50%;
-    margin-left: -450px;
-    display: flex;
-    justify-content: space-around;
-
-    .data-item {
-      width: 140px;
-      display: flex;
-      align-items: center;
-    }
-
-    .data-icon {
-      width: 40px;
-      height: 40px;
-      margin-right: 9px;
-    }
-
-    .data-name {
-      font-size: 18px;
-      color: #50E3C2;
-    }
-
-    .data {
-      font-size: 24px;
-      color: #FFFFFF;
-    }
-  }
-
-  .energy {
-    .energy-box {
-      display: flex;
-      justify-content: space-between;
-    }
-
-    .energy-item {
-      margin-top: 15px;
-      position: relative;
-      width: 230px;
-      height: 38px;
-      font-size: 16px;
-      color: #fff;
-      text-align: center;
-      line-height: 38px;
-
-      &::before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        opacity: 0.59;
-        background: rgba(74, 74, 74, 0.61);
-        border: 1px solid rgba(147, 226, 239, 1);
-        box-shadow: inset 0px 0px 39px 2px rgba(92, 188, 218, 0.82);
-      }
-    }
-
-    .item-content {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: space-between;
-    }
-
-    .energy-left {
-      padding-left: 10px;
-    }
-
-    .energy-right {
-      padding-right: 10px;
-      display: flex;
-      align-items: center;
-      font-weight: 900;
-      font-size: 18px;
-    }
-
-    .icon-up-down {
-      padding-right: 6px;
-    }
-
-    .wrap-cont {
-      height: 191px;
-    }
-  }
-
-  .equipment {
-    .equipment-box {
-      display: flex;
-      justify-content: space-between;
-    }
-
-    .equipment-item {
-      margin-top: 15px;
-      position: relative;
-      width: 140px;
-      height: 55px;
-      font-size: 16px;
-      color: #fff;
-      text-align: center;
-      line-height: 30px;
-
-      &::before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        opacity: 0.59;
-        background: rgba(74, 74, 74, 0.61);
-        border: 1px solid rgba(147, 226, 239, 1);
-        box-shadow: inset 0px 0px 39px 2px rgba(92, 188, 218, 0.82);
-      }
-    }
-
-    .item-content {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-    }
-
-    .equipment-data {
-      color: #00BEE2;
-      font-size: 18px;
-      line-height: 18px;
-
-      &.error {
-        color: #FF5959;
-      }
-    }
-
-    .wrap-cont {
-      height: 176px;
-    }
-  }
-
-  .ratio-wrap {
-    position: absolute;
-    top: 75px;
-    right: 18px;
-    display: flex;
-    text-align: center;
-
-    .data {
-      padding-top: 8px;
-      display: flex;
-      justify-content: center;
-      font-size: 21px;
-    }
-
-    .icon-up-down {
-      width: 20px;
-      height: 20px;
-      padding: 0 7px;
-    }
-
-    .ratio-box {
-      width: 148px;
-      height: 66px;
-      // border: 2px solid rgba(54, 255, 251, 0.22);
-      background: url(../assets/home/ratio-bg.png) no-repeat center 0;
-      background-size: 100%;
-    }
-
-    .ratio-box+.ratio-box {
-      margin-left: 29px;
-    }
-
-    // .ratio-box:nth-child(1) {
-    //   width: 138px;
-    // }
-
-    // .ratio-box:nth-child(2) {
-    //   width: 108px;
-    // }
-
-    // .ratio-box:nth-child(3) {
-    //   width: 128px;
-    // }
-
-    // .ratio-box:nth-child(4) {
-    //   width: 131px;
-    // }
-
-    .ratio-name {
-      font-size: 18px;
-    }
-  }
-
   .handle-content {
     height: 100px;
     width: 13px;
