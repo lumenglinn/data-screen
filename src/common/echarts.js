5e8b73184449edd09330a750718c0e0a425027db
lumenglinn <35032663+lumenglinn@users.noreply.github.com>
2023-05-06 18:27:29 +0800

    Update echarts.js

--- src/common/echarts.js
+++ src/common/echarts.js
@@ -1,7 +1,6 @@
 // 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
 import * as echarts from "echarts/core";
  
-/** 引入柱状图and折线图图表，图表后缀都为 Chart  */
 import { BarChart, LineChart } from "echarts/charts";
  
 // 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
@@ -11,6 +10,7 @@ import {
   GridComponent,
   DatasetComponent,
   TransformComponent,
+  DataZoomComponent 
 } from "echarts/components";
  
 // 标签自动布局，全局过渡动画等特性
@@ -26,6 +26,7 @@ echarts.use([
   GridComponent,
   DatasetComponent,
   TransformComponent,
+  DataZoomComponent,
   BarChart,
   LabelLayout,
   UniversalTransition,
