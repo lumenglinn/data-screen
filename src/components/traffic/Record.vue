a72fbbf8fc388532c37f8c54155c6833ef33925f
lumenglinn <35032663+lumenglinn@users.noreply.github.com>
2023-05-10 18:08:28 +0800

    Update Record.vue

--- src/components/traffic/Record.vue
+++ src/components/traffic/Record.vue
@@ -1,49 +1,27 @@
 
 
 <template>
-  <div class="wrap services">
+  <div class="wrap record">
     <div class="wrap-title">服务质量</div>
     <div class="wrap-cont">
-      <div class="service-head">
-        <div class="service-item total">
-          <div class="service-icon"></div>
-          <div class="service-name">总任务</div>
-          <div class="service-data">231</div>
-        </div>
-        <div class="service-item completed">
-          <div class="service-icon"></div>
-          <div class="service-name">已完成</div>
-          <div class="service-data">231</div>
-        </div>
-        <div class="service-item incomplete">
-          <div class="service-icon"></div>
-          <div class="service-name">未完成</div>
-          <div class="service-data">231</div>
-        </div>
-        <div class="service-item fail">
-          <div class="service-icon"></div>
-          <div class="service-name">不合格</div>
-          <div class="service-data">231</div>
-        </div>
-      </div>
-      <div class="list-table equipment">
+      <div class="list-table">
         <div class="list-head">
-          <div class="list-head-item">任务号</div>
-          <div class="list-head-item">项目</div>
+          <div class="list-head-item">编号</div>
+          <div class="list-head-item">车牌号</div>
+          <div class="list-head-item">卡口编号</div>
           <div class="list-head-item time">时间</div>
-          <div class="list-head-item">是否合格</div>
+          <div class="list-head-item">车辆类型</div>
         </div>
         <div class="list-body">
           <swiper :slides-per-view="5" :slides-per-group="5" :autoplay="{ delay: 2000, disableOnInteraction: false }"
             :speed="500" :space-between="5" :direction="'vertical'" :scrollbar="{ draggable: false }" :loop="true"
             :modules="modules" style="height: 7.854167vw" ref="mySwiper">
             <swiper-slide v-for="(item, index) in dataSource" :key="index" class="list-data">
-              <div class="data-item">{{ item.no }}</div>
+              <div class="data-item">{{ index+1 }}</div>
               <div class="data-item">{{ item.name }}</div>
+              <div class="data-item">{{ item.no }}</div>
               <div class="data-item time">{{ item.time }}</div>
-              <div class="data-item">
-                <div class="result" :class="{ 'error': item.result === 1 }"></div>
-              </div>
+              <div class="data-item">小轿车</div>
             </swiper-slide>
           </swiper>
         </div>
@@ -70,51 +48,12 @@ const props = defineProps({
 
 
 <style lang="scss" scoped>
-.services {
+.record {
   width: 100%;
 
   .wrap-cont {
     padding-top: 13px;
     height: 231px;
-  }
-
-  .service-head {
-    width: 457px;
-    height: 26px;
-    margin: 0 auto 0;
-    display: flex;
-    line-height: 26px;
-    background: rgba(10, 62, 71, 0.8);
-    border-radius: 4px;
-    font-size: 14px;
-    color: #ffffff;
-  }
-
-  .service-item {
-    width: 25%;
-    display: flex;
-    font-weight: 200;
-  }
-
-  .service-icon {
-    width: 14px;
-    height: 8px;
-    margin-top: 9px;
-    margin-left: 7px;
-    margin-right: 9px;
-    background: #7ED321;
-  }
-
-  .completed .service-icon {
-    background: #00FFFF;
-  }
-
-  .incomplete .service-icon {
-    background: #9B9B9B;
-  }
-
-  .fail .service-icon {
-    background: #E62A44;
   }
 
   .service-data {
@@ -127,23 +66,12 @@ const props = defineProps({
 
   .data-item,
   .list-head-item {
-    width: 22.3%;
+    width: 19%;
   }
 
   .data-item.time,
   .list-head-item.time {
-    width: 33%;
-  }
-
-  .result {
-    width: 14px;
-    height: 8px;
-    margin: 10px auto 0;
-    background: #00FFFF;
-
-    &.error {
-      background: #E62A44;
-    }
+    width: 24%;
   }
 }
 </style>
