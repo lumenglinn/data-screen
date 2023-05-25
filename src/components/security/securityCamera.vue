19199f517936685ea2e92840bc2e1abac9154a4d
lumenglinn <35032663+lumenglinn@users.noreply.github.com>
2023-05-25 16:55:52 +0800

    Update securityCamera.vue

--- src/components/security/securityCamera.vue
+++ src/components/security/securityCamera.vue
@@ -1,23 +1,34 @@
-
-
 <template>
-  <div class="center-box security-camera" v-show="isShow">
-    <swiper :slides-per-view="5" :slides-per-group="5" :autoplay="{ delay: 2000, disableOnInteraction: false }"
-      :speed="500" :space-between="5" :direction="'vertical'" :scrollbar="{ draggable: false }" :loop="true"
-      :modules="modules" style="height: 8.64167vw" ref="mySwiper">
-      <swiper-slide v-for="(item, index) in dataSource" :key="index" @click="switchStatus">
-        <div class="table-box">
-          <div class="data-item">{{ item.degree }}</div>
-          <div class="data-item">{{ item.equipment }}</div>
-          <div class="data-item">{{ item.description }}</div>
-          <div class="data-item">{{ item.time }}</div>
-          <div class="data-item" v-if="operation === 1">待确认</div>
+  <div class="center-box security-fire" v-show="isShow">
+    <div class="wrap">
+      <div class="wrap-title">离线相机</div>
+    </div>
+    <div class="center-table">
+      <div class="list-table">
+        <div class="list-head">
+          <div class="list-head-item">分区</div>
+          <div class="list-head-item">位置</div>
+          <div class="list-head-item">描述</div>
+          <div class="list-head-item">报警时间</div>
+          <div class="list-head-item">IP号</div>
         </div>
-      </swiper-slide>
-  </swiper>
-  <!-- <div class="table-box">
-
-      </div> -->
+        <div class="list-body">
+          <swiper :slides-per-view="5" :slides-per-group="5" :autoplay="{ delay: 2000, disableOnInteraction: false }"
+            :speed="500" :space-between="5" :direction="'vertical'" :scrollbar="{ draggable: false }" :loop="true"
+            :modules="modules" style="height: 8.64167vw" ref="mySwiper">
+            <swiper-slide v-for="(item, index) in dataSource" :key="index" @click="viewData">
+              <div class="list-data">
+                <div class="data-item">{{ item.area }}</div>
+                <div class="data-item">{{ item.position }}</div>
+                <div class="data-item">{{ item.description }}</div>
+                <div class="data-item">{{ item.time }}</div>
+                <div class="data-item">{{ item.IP }}</div>
+              </div>
+            </swiper-slide>
+          </swiper>
+        </div>
+      </div>
+    </div>
     <div class="close" @click="handleModel(false)"></div>
   </div>
 </template>
@@ -29,6 +40,17 @@ import { Autoplay, Navigation, Pagination, A11y } from 'swiper';
 import 'swiper/css';
 const modules = [Autoplay, Pagination, Navigation, A11y];
 const isShow = ref(false)
+
+const props = defineProps({
+  dataSource: {
+    type: Array,
+    default: []
+  },
+  energyObj: {
+    type: Object,
+    default: {}
+  }
+})
 
 const handleModel = (action) => {
   isShow.value = action
@@ -42,69 +64,7 @@ defineExpose({
 
 
 <style lang="scss" scoped>
-.car-center {
-  .img-box {
-    padding: 55px 37px 0;
-
-    img {
-      width: 100%;
-    }
-  }
-
-  .car-info {
-    padding: 26px 38px;
-    display: flex;
-    justify-content: space-between;
-
-    .car-info-item {
-      width: 50%;
-    }
-
-    .detail-content {
-      padding: 15px 0 0 15px;
-    }
-
-    .detail {
-      display: flex;
-      font-size: 16px;
-      font-weight: 700;
-      line-height: 30px;
-    }
-
-    .label {
-      width: 80px;
-    }
-
-    .info-title {
-      display: block;
-      position: relative;
-      width: 484px;
-      height: 29px;
-      line-height: 29px;
-      padding-left: 31px;
-      color: #fff;
-      font-size: 18px;
-      font-weight: 700;
-      background-image: linear-gradient(270deg, rgba(92, 188, 218, 0) 30%, #5cbcda 100%);
-
-      &::before {
-        content: "";
-        width: 9px;
-        height: 18px;
-        position: absolute;
-        left: 15px;
-        top: 5px;
-        background-color: #fff;
-      }
-    }
-  }
-
-  .small-img {
-    margin: 24px;
-    width: 150px;
-    height: 103px;
-  }
-}
+.security-fire {}
 </style>
 
 
